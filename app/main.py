fdd70d0ebec9a6aafafef69a21e116fd32468212
nikunjjadav123 <dirensj@gmail.com>
2025-11-17 22:33:41 +0530

    Initialize main.py file

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+todos = []
+
+class Todo(BaseModel):
+    id: int
+    task: str
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI + Jenkins + Docker!"}
+
+@app.get("/todos")
+def get_todos():
+    return todos
+
+@app.post("/todos")
+def add_todo(todo: Todo):
+    todos.append(todo)
+    return {"msg": "Todo added", "todo": todo}
