f8493c12c269eb949447d07bdfb111615b6f565f
nikunjjadav123 <dirensj@gmail.com>
2025-11-18 14:09:53 +0530

    Modified Jenkinsfile

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class Todo(BaseModel):
 
 @app.get("/")
 def home():
-    return {"message": "FastAPI + Jenkins + Docker! + For Jenkins Only For Testing Purposes......"}
+    return {"message": "FastAPI + Jenkins + Docker! + For Jenkins Only For Testing Purposes Only......"}
 
 @app.get("/todos")
 def get_todos():
